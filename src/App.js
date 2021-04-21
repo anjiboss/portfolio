1bec139a5f75fe45368a82d6e36aa164e566d0ee
Anjiboss <nhu.nhu.face@gmail.com>
2021-04-21 13:10:45 +0900

    setting nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import Loading from "./Component/Loading";
+import NavBar from "./Component/NavBar";
 
 function App() {
+  const [image, setImage] = useState("");
+  useEffect(() => {
+    fetch("http://localhost:5000/api/img/")
+      .then((respone) => respone.blob())
+      .then((img) => {
+        const imgURL = URL.createObjectURL(img);
+        setImage(imgURL);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <div className="container">
+        {image === "" ? (
+          <Loading />
+        ) : (
+          <img className="profile-img" src={image} alt="Profile" />
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/Component/NavBar.js
@@ -0,0 +1,26 @@
+const NavBar = () => {
+  return (
+    <>
+      <nav className="nav-bar">
+        <ul>
+          <li>
+            <h4>
+              <a href="http://localhost:3000">Bui Tuyen</a>
+            </h4>
+          </li>
+          <li>
+            <h4>
+              <a href="http://localhost:3000/about">About Me</a>
+            </h4>
+          </li>
+          <li>
+            <h4>
+              <a href="http://localhost:3000/project">My Project</a>
+            </h4>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+export default NavBar;
